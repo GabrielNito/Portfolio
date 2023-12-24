bb2bd0e31abf46d8900ba49bd8775b502bcabb0f
Gabriel Nito <nito.gabriel22@gmail.com>
2023-12-24 17:24:19 -0300

    24-12-2023

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: "./server.js",
+  entry: "./main.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
